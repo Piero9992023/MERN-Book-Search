5176c9f59beaf0bd6a9a7151cf6adc6ed4a714a1
Piero Rodriguez <piero@Pieros-MacBook-Pro.local>
2023-12-20 14:35:54 -0500

    completed adding mutations/queries, added mutations in login/signup/pages folder

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client'
 
-import { createUser } from '../utils/API';
+import { ADD_USER } from '../utils/mutations'
+// import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -9,6 +11,9 @@ const SignupForm = () => {
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
   // set state for form validation
   const [validated] = useState(false);
+
+  const [addUser, { error, data }] = useMutation(ADD_USER);
+
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
@@ -27,16 +32,16 @@ const SignupForm = () => {
       event.stopPropagation();
     }
 
+    if(error) {
+      throw new Error("something went wrong!!");
+    }
+
     try {
-      const response = await createUser(userFormData);
+      const { data } = await addUser({
+        variables: { ...userFormData },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
